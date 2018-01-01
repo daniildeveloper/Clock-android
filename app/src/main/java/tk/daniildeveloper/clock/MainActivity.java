d4b19172ab6e15b5d4fdcc8ba49b199f14e46803
Daniil Borovkov <daniilborowkow@yandex.ru>
2018-01-01 12:17:46 +0600

    First part of tutorial listened.

--- app/src/main/java/tk/daniildeveloper/clock/MainActivity.java
+++ app/src/main/java/tk/daniildeveloper/clock/MainActivity.java
@@ -1,5 +1,6 @@
 package tk.daniildeveloper.clock;
 
+import android.app.AlarmManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button alarmOn, alarmOff;
+    TextView updateText;
+    TimePicker timePicker;
+    AlarmManager alarmManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +27,30 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+//        initialize created components
+        alarmOn = (Button) findViewById(R.id.alarm_on);
+        alarmOff = (Button) findViewById(R.id.alarm_off);
+        updateText = (TextView) findViewById(R.id.updateText);
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+//        add listeners for buttons
+        alarmOn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                setTimeText("Будильник включен");
             }
         });
+        alarmOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setTimeText("Будильник выключен");
+            }
+        });
+    }
+
+    private void setTimeText(String s) {
+        updateText.setText(s);
     }
 
     @Override
